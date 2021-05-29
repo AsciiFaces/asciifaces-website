fcfaf534bfb19f9093dc66f4f7e866a4dbd69821
irfan marzuki <irfnmzk@gmail.com>
2021-05-29 23:35:29 +0700

    add face page

--- src/pages/face/[id].tsx
+++ src/pages/face/[id].tsx
@@ -1,19 +1,34 @@
 import { useRouter } from "next/router";
 import { Card, Container } from "@components/common";
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import { ParsedUrlQuery } from "querystring";
 
-export default function FacePage() {
-  const {
-    query: { id },
-  } = useRouter();
+export const getStaticPaths: GetStaticPaths = async () => {
+  return { paths: [], fallback: "blocking" };
+};
 
+interface IParams extends ParsedUrlQuery {
+  id: string;
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { id } = context.params as IParams;
+  return { props: { id } };
+};
+
+export default function FacePage({
+  id,
+}: IParams): InferGetStaticPropsType<typeof getStaticProps> {
   return (
     <Container>
       <Card>
         <div className="w-full flex flex-col items-center justify-center">
           <h2 className="text-3xl font-semibold">Faces #{id}</h2>
           <img
-            className="shadow-superiority my-6"
+            className="shadow-superiority my-6 bg-black"
             src={`https://dummyimage.com/350x350/000/fff`}
+            height={350}
+            width={350}
             alt=""
           />
           <h3 className="mb-4">Owned By 0x</h3>
